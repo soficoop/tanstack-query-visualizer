c8005c58135ce98d4dc2e1ecaf8e99e4d45ef48e
arikhativa <arikhativa@gmail.com>
2026-01-26 12:59:34 +0200

    comp err

--- src/lib/queryKey/index.ts
+++
@@ -1,5 +0,0 @@
-import { mergeQueryKeys } from "@lukemorales/query-key-factory";
-import { house } from "./house";
-import { resident } from "./resident";
-
-export const queries = mergeQueryKeys(house, resident);
